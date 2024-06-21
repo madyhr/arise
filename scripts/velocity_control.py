087c1702011c91b84543f8a7cc8cda5d6086df82
madyhr <marcusdyhr@gmail.com>
2024-06-21 17:55:17 +0200

    minor vel_control.py changes

--- scripts/velocity_control.py
+++ scripts/velocity_control.py
@@ -9,14 +9,16 @@ class velocity_control(Node):
     def __init__(self):
         super().__init__("velocity_control_node")
         self.get_logger().info("cmd_vel publisher node initialized.")
+        self.i = 0
         self.timer = self.create_timer(0.5, self.send_velocity_command)
         self.cmd_vel_pub = self.create_publisher(
             Twist, "hexapod/cmd_vel", 10
         )
     
     def send_velocity_command(self):
+        self.i += 0.1
         msg = Twist()
-        msg.linear.x = 1.0
+        msg.linear.x = 1.0 + self.i
         msg.angular.z = 1.5
         self.cmd_vel_pub.publish(msg)
 
