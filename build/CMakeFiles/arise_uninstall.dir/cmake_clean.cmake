470060a8e52888745ad00abe59c6595d6aafc056
madyhr <marcusdyhr@gmail.com>
2024-06-21 20:43:08 +0200

    surely this will fix it

--- build/CMakeFiles/arise_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/arise_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/arise_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
